f868031bff1290a8dc52afed8a501b8acbfe3f67
Dominik Wiecek <domis.wiecek@gmail.com>
2018-12-09 23:25:55 +0000

    First commit

--- app/src/main/java/com/example/domis/assignment2/GameActivity.java
+++ app/src/main/java/com/example/domis/assignment2/GameActivity.java
@@ -61,6 +61,7 @@ public class GameActivity extends AppCompatActivity {
                     Log.e("Height: ", y + "");
                     balloonButton.setX(x);
                     balloonButton.setY(y);
+                    //l;k;k
                     if(score <= 10) {
                         score++;
                     }
